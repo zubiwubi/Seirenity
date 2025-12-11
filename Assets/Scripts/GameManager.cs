a8be156c49c100d69f7647fa2f3487923b9d70c9
Zubaydah <z.koelemeij@gmail.com>
2025-12-11 13:22:00 +0100

    First Commit
    First commit with all the added assets up until 11th Dec. 2025.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject environmentTransform;
+
+    private void Update()
+    {
+        //environmentTransform.transform.position;
+    }
+}
